08cc73c945d056b9f6d4bf292d17cb793a98dda3
Gabe <glebzex@mail.ua>
2020-07-05 15:55:25 +0300

    Add config locations

---
+++ Assets/desExt/Runtime/ConfigLocations.cs
@@ -0,0 +1,18 @@
+namespace desExt.Runtime
+{
+    public static class ConfigLocations
+    {
+        public const string ConfigFolderDatabasePath = "Assets/Resources/" + ConfigFolderResourcePath;
+        public const string ConfigFolderResourcePath = "desExt/Config/";
+
+        public static string GetAssetResourcePath(string assetName)
+        {
+            return ConfigFolderResourcePath + assetName;
+        }
+
+        public static string GetAssetDatabasePath(string assetName)
+        {
+            return ConfigFolderDatabasePath + assetName + ".asset";
+        }
+    }
+}
