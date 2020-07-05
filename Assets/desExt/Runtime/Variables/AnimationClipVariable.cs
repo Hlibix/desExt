76e687d612bed37f4b8bbac2c847af7896893f33
Gabe <glebzex@mail.ua>
2020-07-05 15:51:15 +0300

    Add animation clip variable

---
+++ Assets/desExt/Runtime/Variables/AnimationClipVariable.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace desExt.Runtime.Variables
+{
+    [CreateAssetMenu(menuName = DesExtNames.VariablesMenuName + "Animation Clip Variable")]
+    public class AnimationClipVariable : BaseTypeVariable<AnimationClip>
+    {
+    }
+}
