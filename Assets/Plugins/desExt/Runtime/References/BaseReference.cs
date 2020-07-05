4713a230ce24c867e5639714bd34e6f38f090baf
Gabe <glebzex@mail.ua>
2020-07-05 17:01:24 +0300

    Update BaseReference.cs
    Now force uses constant value when reference is not set

--- Assets/Plugins/desExt/Runtime/References/BaseReference.cs
+++ Assets/Plugins/desExt/Runtime/References/BaseReference.cs
@@ -1,4 +1,5 @@
 using System;
+using desExt.Runtime.Utils;
 using desExt.Runtime.Variables;
 using UnityEngine;
 
@@ -19,7 +20,25 @@ namespace desExt.Runtime.References
         private bool useConstantValue;
 #pragma warning restore 0649
 
-        public TValue Value => useConstantValue ? constantValue : variableReference.Value;
+        public TValue Value
+        {
+            get
+            {
+                if (useConstantValue)
+                {
+                    return constantValue;
+                }
+
+                if (!useConstantValue && variableReference == null)
+                {
+                    Logging.LogWarning(
+                        $"Reference value of {GetType().Name.Color(Color.blue)} is null! Forcing constant value...");
+                    return constantValue;
+                }
+
+                return variableReference.Value;
+            }
+        }
 
         public void SetConstantValue(TValue value)
         {
